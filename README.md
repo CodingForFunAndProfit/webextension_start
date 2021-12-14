c9d174b8c2222e3d283a3f1cf5c06bdf1eb4e681
CodingForFunAndProfit <sfloxn@gmail.com>
2021-12-14 15:06:58 +0100

    Simple bookmark extension as a starter. + webpack

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Starting project for Web Extensions
 
 web-ext run
 web-ext build
+
+npm install webpack webpack-cli webpack-dev-server web-ext-plugin --save-dev
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const WebExtPlugin = require("web-ext-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    plugins: [new WebExtPlugin({})],
+};
